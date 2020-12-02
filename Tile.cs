97b034fd4961c1c803708989070f8526cbe18a31
pstebl99@mail.ru <pstebl99@mail.ru>
2020-12-02 22:46:09 +0400

    Проверка возможности печали чека

---
+++ Tile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Laba6
+{
+    class Tile
+    {
+        public string Name { get; set; }
+        private readonly string description = null;
+        public int PriceOfOnePack { get; set; }
+
+        public Tile(string name, int price)
+        {
+            this.Name = name;
+            this.PriceOfOnePack = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}    {PriceOfOnePack}";
+        }
+    }
+}
