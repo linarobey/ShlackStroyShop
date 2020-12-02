eeaec0ebb231ab556634ea8308e0d7b78ce0948c
linarobey <linarobey@yandex.ru>
2020-12-02 22:24:07 +0400

    Реализовано отображение суммы покупки на экране, ее обнуление после завершения покупки

--- Purhase.cs
+++ Purhase.cs
@@ -7,6 +7,9 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
