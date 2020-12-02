7bfb0d7208b87df14ab52b67cfef86821d799e91
linarobey <linarobey@yandex.ru>
2020-12-02 20:55:36 +0400

    Реализован ввод размеров

---
+++ Stroy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stroy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
