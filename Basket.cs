b9bd2191deaabd6bcf4eef53ae5db2eba85da5ad
pstebl99@mail.ru <pstebl99@mail.ru>
2020-12-02 22:29:49 +0400

    Реализована возможность добавления информации в корзину

---
+++ Basket.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Laba6
+{
+    class Basket
+    {
+        public int Price { get; set; }
+
+        public Basket()
+        {
+            Price = 0;
+        }
+
+        public void ChangePrice(int count, int price)
+        {
+            Price += count * price;
+        }
+
+        public void PrintCheck()
+        {
+            if (Price < 1)
+                Console.WriteLine("\nЧек не может быть напечатан: корзина пуста.");
+            else
+            {
+                Console.WriteLine("\nЧек напечатан! Корзина очищена.");
+                Price = 0;
+            }
+        }
+    }
+}
